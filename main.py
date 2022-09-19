31680f4ae3e687232adb5dd8eaf2c36fe243aea7
Ray Karyshyn <raykaryshyn@gmail.com>
2022-09-19 12:21:30 -0400

    printed encryption KATs

--- main.py
+++ main.py
@@ -164,6 +164,7 @@ class SDES:
         return self.IPi(L)
 
 
+'''
 mySDES = SDES('0000000000')
 message = '10000000'
 encrypted = mySDES.encrypt(message, 'b')
@@ -171,3 +172,67 @@ decrypted = mySDES.decrypt(encrypted.bin, 'b')
 
 print("Cipher:", encrypted.bin)
 print("Plain: ",decrypted.bin)
+'''
+
+def VarPlaintextKAT():
+    key = '0000000000'
+    sdes = SDES(key)
+    p = BitArray(bin='10000000')
+    table = []
+
+    for _ in range(8):
+        table.append([p.bin, sdes.encrypt(str(p.bin), 'b').bin])
+        p.ror(1)
+
+    return table
+
+def InvPermKAT(tableIn):
+    key = '0000000000'
+    sdes = SDES(key)
+    tableOut = []
+
+    for i in range(8):
+        tableOut.append([tableIn[i][1], sdes.encrypt(tableIn[i][1], 'b').bin])
+
+    return tableOut
+
+def VarKeyKAT():
+    key = BitArray(bin='1000000000')
+    sdes = SDES(str(key.bin))
+    p = '00000000'
+    table = []
+
+    for _ in range(10):
+        table.append([str(key.bin), sdes.encrypt(p, 'b').bin])
+        key.ror(1)
+        sdes = SDES(str(key.bin))
+
+    return table
+
+def PermOpKAT():
+    keys = ['0000000011', '0011001010', '0001011001', '1011001111']
+    p = '00000000'
+    table = []
+
+    for i in range(4):
+        sdes = SDES(keys[i])
+        table.append([keys[i], sdes.encrypt(p, 'b').bin])
+
+    return table
+
+def SubTableKAT():
+    keys = ['0001101101', '0001101110', '0001110000', '0001110001', '0001110110', '0001111000', '0001111001']
+    p = '00000000'
+    table = []
+
+    for i in range(7):
+        sdes = SDES(keys[i])
+        table.append([keys[i], sdes.encrypt(p, 'b').bin])
+
+    return table
+
+print(VarPlaintextKAT())
+print(InvPermKAT(VarPlaintextKAT()))
+print(VarKeyKAT())
+print(PermOpKAT())
+print(SubTableKAT())
