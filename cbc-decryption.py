c86fcc1adb5d1298ed6661d9b439629ebd72cab6
Ray Karyshyn <raykaryshyn@gmail.com>
2022-09-23 10:08:20 -0400

    CBC, simplify bytearray to BitArray transformation

--- cbc-decryption.py
+++ cbc-decryption.py
@@ -12,15 +12,14 @@ prev = iv
 plain = ''
 for i in range(len(cipher_bytes)):
     # Take next byte from cipher_bytes and transform into an 8-bit BitArray.
-    encrypted_byte = BitArray(uint=int.from_bytes(
-        cipher_bytes[i:i+1], 'big', signed=False), length=8)
+    encrypted_byte = BitArray(bytes=cipher_bytes[i:i+1])
     # Call the decrypt function on the byte.
     pre_decrypted_byte = myDSDES.decrypt(encrypted_byte.bin, 'b')
     # Finish the CBC decryption by XORing by the previous encrypted_byte (or IV).
     decrypted_byte = prev ^ pre_decrypted_byte
-    # Append the ASCII character value of decrypted_byte to plain
+    # Append the ASCII character value of decrypted_byte to plain.
     plain += chr(int((decrypted_byte).hex, base=16))
-    # Save the current encrypted_byte for decrypting the next byte in cipher_bytes
+    # Save the current encrypted_byte for decrypting the next byte in cipher_bytes.
     prev = encrypted_byte
 
 print('CBC DS-DES Decryption')
