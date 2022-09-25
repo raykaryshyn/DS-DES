8cfd3f119faa6d77230e801647691c1d78731b85
Ray Karyshyn <raykaryshyn@gmail.com>
2022-09-25 15:08:45 -0400

    improve mitm algorithm

--- meet-in-the-middle.py
+++ meet-in-the-middle.py
@@ -7,6 +7,7 @@ plaintext = '4272757465'
 ciphertext = '52f0be698a'
 
 possible_ciphertexts = []
+possible_cipher_keys = []
 found_keys = []
 saved_sdes = []
 
@@ -16,43 +17,59 @@ print('Attacking...\n')
 
 start_time = time.time()
 
+# Loops over all possible keys for the first S-DES (encryption).
 for i in range(0b10000000000):
     current_key = BitArray(uint=i, length=10)
     current_sdes = SDES(current_key.bin, 'b')
-    current_ciphertext = []
+    # Applies the current_sdes encryption to all plaintext blocks.
+    current_ciphertext = ''
     for j in range(0, len(plaintext), 2):
-        current_ciphertext.append(current_sdes.encrypt(plaintext[j:j+2]).bin)
-    possible_ciphertexts.append((current_key.bin, ''.join(current_ciphertext)))
+        current_ciphertext += current_sdes.encrypt(plaintext[j:j+2]).bin
+    # Saves the current key and encryption result.
+    possible_ciphertexts.append(current_ciphertext)
+    possible_cipher_keys.append(current_key.bin)
+    # Saves the current_sdes for use in the second DS-DES round.
     saved_sdes.append(current_sdes)
 
+# Loops over all possible keys for the second S-DES (decryption).
 for i in range(0b10000000000):
     current_key = BitArray(uint=i, length=10)
     current_sdes = saved_sdes[i]
-    current_plaintext = []
+    current_plaintext = ''
+    # Applies the current_sdes decryption to all ciphertext blocks.
     for j in range(0, len(ciphertext), 2):
-        current_plaintext.append(current_sdes.decrypt(ciphertext[j:j+2]).bin)
-    for x in possible_ciphertexts:
-        if (x[1] == ''.join(current_plaintext)):
-            found_keys.append(x[0])
-            found_keys.append(current_key.bin)
-            break
+        current_plaintext += current_sdes.decrypt(ciphertext[j:j+2]).bin
+    # Try to find a match of current_plaintext in possible_ciphertexts.
+    try:
+        found_index = possible_ciphertexts.index(current_plaintext)
+        # First key is from the first S-DES.
+        found_keys.append(possible_cipher_keys[found_index])
+        # Second key is from the current (second) S-DES.
+        found_keys.append(current_key.bin)
+        break
+    except ValueError:
+        pass
 
 end_time = time.time() - start_time
 
-print('Found keys:', found_keys)
-hex_key = BitArray(bin=''.join(found_keys)).hex
-print('20-bit combined key as hex:', hex_key, '\n')
+if len(found_keys) != 2:
+    print('Unable to find the 20-bit key bundle.')
+    exit()
 
-print('Testing the found key with the given plaintext "' +
+print('Found key bundle:', found_keys)
+hex_key = BitArray(bin=''.join(found_keys)).hex
+print('20-bit combined key bundle as hex:', hex_key, '\n')
+
+print('Testing the found key bundle with the given plaintext "' +
       ''.join([chr(int(plaintext[x:x+2], base=16)) for x in range(0, len(plaintext), 2)]) +
       '" (' + plaintext + '):')
 print('Provided ciphertext:\n\t', ciphertext)
 
 test_dsdes = DSDES(''.join(found_keys), 'b')
-test_ciphertext = ""
+test_ciphertext = ''
 for j in range(0, len(plaintext), 2):
     test_ciphertext += test_dsdes.encrypt(plaintext[j:j+2]).hex
 
-print('Generated ciphertext with provided plaintext and found key (' +
+print('Generated ciphertext with provided plaintext and found key bundle (' +
       hex_key + '):\n\t', test_ciphertext)
 print('\nCompleted in', '%.2f' % end_time, 'seconds.')
